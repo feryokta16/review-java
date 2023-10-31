e73785f1768d33d2c1618eaee1feb21d5a263017
fery <feryokta1908@gmail.com>
2023-10-31 10:58:47 +0700

    Tugas Review Java

--- src/main/java/org/example/SoalNomer2.java
+++ src/main/java/org/example/SoalNomer2.java
@@ -14,16 +14,6 @@ public class SoalNomer2 {
     public static void main(String[] args) {
         DataAnalist dataAnalist = new DataAnalist();
 
-//        System.out.println("Jawaban nomer 1");
-//        pembahgian(100);
-//        System.out.println("Jawaban Nomer 2");
-//        Perulangan(1,9);
-//        System.out.println("Jawaban Nomer 3");
-//        reArrage(dataAnalist.T1_1, dataAnalist.T2_1);
-//        System.out.println("--------------");
-//        reArrage(dataAnalist.T1_2, dataAnalist.T2_2);
-//        System.out.println("--------------");
-//        reArrage(dataAnalist.T1_3, dataAnalist.T2_3);
     }
     static void  perulangan(int start, int end) {
         while (start <= end) {
